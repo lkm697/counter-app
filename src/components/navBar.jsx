ea8167c1377f2e5b890af8456053323486fead06
Leon Mwangi <38433520+lkm697@users.noreply.github.com>
2020-11-19 01:57:05 -0600

    Update navBar.jsx

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -4,11 +4,7 @@ class NavBar extends Component {
 
     render() { 
         return (
-<<<<<<< HEAD
         <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-=======
-        <nav className="navbar navbar-light bg-light">
->>>>>>> 5329c3db4033b55f9eaa3fc35033cc9bdbd869ff
             <a className="navbar-brand" href="https://www.google.com">
                 NavBar{" "}
                 <span className="badge badge-pill badge-secondary">
